b532c4103f9bd8b1ee5f513982624abb2ffd04db
Pavel Blinov <blinov.pavel@gmail.com>
2021-06-14 17:19:52 +0300

    Axises

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button v-on:click="drawRect">Function</button>
-    <canvas id="canvas" width="600" height="600" ></canvas>
+
+    <div id="rectangle" class="mainControls">
+      <button v-on:click="drawRect">Function</button>
+    </div>
+    <canvas id="canvas" width="600" height="600" class="mainCanvas"></canvas>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -15,39 +19,51 @@ export default class HelloWorld extends Vue {
   vueCanvas!: object
 
   mounted(): void {
-    const canvasElement = document.getElementById("canvas");
-    this.vueCanvas = canvasElement.getContext("2d");
+    this.vueCanvas = document
+        .getElementById("canvas")
+        .getContext("2d");
   }
 
   drawRect() {
+    const ctx = this.vueCanvas;
+
     const canvasWidth = 600;
     const marginX = 10;
     const canvasHeight = 600;
     const marginY = 10;
 
     const maxX = 2 * Math.PI;
-    const aX = (canvasWidth - 2 * marginX) / (2 * maxX);
-    const bX = canvasWidth / 2;
-    const convertWidth = function (width: number): number{
+    const convertWidth = function (width: number): number {
+      const aX = (canvasWidth - 2 * marginX) / (2 * maxX);
       return width * aX;
     }
     const convertX = function (x: number): number {
+      const bX = canvasWidth / 2;
       return convertWidth(x) + bX
     }
 
     const maxY = 1;
-    const aY = (canvasHeight - 2 * marginY) / (2 * maxY);
-    const bY = canvasHeight / 2;
     const convertHeight = function (height: number): number {
+      const aY = (canvasHeight - 2 * marginY) / (2 * maxY);
       return height * aY;
     }
     const convertY = function (y: number): number {
+      const bY = canvasHeight / 2;
       return -convertHeight(y) + bY
     }
 
-    const ctx = this.vueCanvas;
+
     const rect = function (x: number, y: number, width: number, height: number): void {
       ctx.rect(convertX(x), convertY(y), convertWidth(width), convertHeight(height));
+    }
+    const moveTo = function (x: number, y: number): void {
+      ctx.moveTo(convertX(x), convertY(y));
+    }
+    const lineTo = function (x: number, y: number): void {
+      ctx.lineTo(convertX(x), convertY(y));
+    }
+    const fillText = function (str: string, x: number, y: number): void {
+      ctx.fillText(str, convertX(x), convertY(y));
     }
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
@@ -56,123 +72,83 @@ export default class HelloWorld extends Vue {
     ctx.strokeStyle = '#22695c';
     ctx.lineWidth = 2;
 
-    const xb = 110; //Коодринаты точки В (см в записи)
-    const yb = 10;
+    //ось ОХ
+    moveTo(maxX, 0);
+    lineTo(-maxX, 0);
 
-    const xd = 460; //Коодринаты точки D (см в записи)
-    const yd = 360;
-
-    const AB = 360; // длина стороны прямоугольника
-    const CD = 460; // длина стороны прямоугольника
-    const CoordinateAxis = 300; //длина оси координат
+    //ось ОУ
+    moveTo(0, maxY);
+    lineTo(0, -maxY);
 
 
-    ctx.moveTo((xb+xd)/2, (yb+yd)/2); //ось ОУ (-)
-    ctx.lineTo((xb+xd)/2, AB - 30);
-    ctx.moveTo((xb+xd)/2, (yb+yd)/2); //ось ОУ (+)
-    ctx.lineTo((xb+xd)/2, AB - 330);
-
-    ctx.moveTo((xb+xd)/2, AB - 330) //стрелки oy
-    ctx.lineTo(CoordinateAxis/2 + 120, AB/16 + AB - 330);
-    ctx.moveTo((xb+xd)/2, AB - 330) //стрелки
-    ctx.lineTo(CoordinateAxis/2 + AB/2 - 30,AB/16 + AB - 330);
-
-    ctx.moveTo((xb+xd)/2, (yb+yd)/2);//ось ОХ (+)
-    ctx.lineTo(430, (yb+yd)/2);
-    ctx.moveTo((xb+xd)/2, (yb+yd)/2);//ось ОХ (-)
-    ctx.lineTo(130, (yb+yd)/2);
-
-    ctx.moveTo(450, (yb+yd)/2)//стрелки ox
-    ctx.lineTo(430, 190);
-    ctx.moveTo(450, (yb+yd)/2)//стрелки
-    ctx.lineTo(430, 170);
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.strokeStyle = '#16967f';
     ctx.lineWidth = 1;
     //Цикл для рисования сетки по ОХ
-    for(let x1 = 3; x1 < 12; x1++) {
-
-      ctx.moveTo(x1*37 + 6.9*(Math.PI),54);
-      ctx.lineTo(x1*37 + 6.9*(Math.PI), 305);
-      ctx.stroke();
+    for (let x1 = -maxX; x1 < maxX; x1 = x1 + Math.PI) {
+      moveTo(x1, -maxY);
+      lineTo(x1, maxY);
     }
 
     //Цикл для рисования сетки по ОY
-    for(let y1 = 1; y1 < 12; y1++) {
-      ctx.moveTo(132,y1*25 + 30);
-      ctx.lineTo(430, y1*25 + 30);
-      ctx.stroke();
+    for (let y1 = -maxY; y1 < maxY; y1++) {
+      moveTo(-maxX, y1);
+      lineTo(maxX, y1);
     }
 
+    ctx.stroke();
+
+    //график
     ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#22695c';
-    ctx.moveTo(132,180);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#29a0a4';
+    moveTo(-maxX,0);
     let x = 0;
-    let y = 119;
-    const x2 = 132;
-    const y2 = 180;
-    const m = 7.5*Math.PI
-    for (x = 0; x<12.6; x = x+1/m)
+    let y = 0;
+    for (x = -maxX; x <= maxX; x = x+1/maxX)
     {
-      y = Math.sin(-x);
-      ctx.lineTo(x*m+x2,y*m+y2);
-
-
+      y = Math.sin(x);
+      lineTo(x,y);
     }
     ctx.stroke();
 
+    //название осей
     ctx.beginPath();
-    ctx.strokeStyle = 'black';
-    ctx.font = "20px serif";
-
-    ctx.fillText("\u03C0", 355.5, 179.5);// число Пи по оси ОХ+
-
-    ctx.fillText("X", 434, 170);//Ox
-    ctx.fillText("O", 280, 180);//O
-    ctx.fillText("Y", 290, 40);//Oy
-
-    ctx.fillText("\u03C0", 318.25, 179.5); // число Пи/2
-    ctx.fillText("\u2013", 319.5, 189, 600);
-    ctx.fillText("2", 319.5, 203);
-
-    ctx.fillText("2" + "\u03C0", 430, 200);    // число 2Пи
-
-    ctx.fillText("3"+"\u03C0", 392.75, 179.5); // число 3Пи/2
-    ctx.fillText("\u2013" + "\u2013", 393.5, 189);
-    ctx.fillText("2", 397, 203);
-
-    ctx.fillText("-"+"\u03C0", 206.5, 179.5);// число Пи по оси ОХ-
-
-    ctx.fillText("-"+"\u03C0", 243.75, 179.5); // число -Пи/2
-    ctx.fillText("\u2013"+"\u2013", 245, 189, 600);
-    ctx.fillText("2", 248, 203);
-
-    ctx.fillText("-"+"3"+"\u03C0", 169.25, 179.5); // число -3Пи/2
-    ctx.fillText("\u2013" + "\u2013", 173, 189);
-    ctx.fillText("2", 177, 203);
-
-    ctx.fillText("-" + "2" + "\u03C0", 134, 200);    // число -2Пи
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#08191c";
+    ctx.font = '10px cursive';
+    for (let i = -2*maxX; i < maxX; i = i + Math.PI/2) {
+      moveTo(i, maxY/10);
+      lineTo(i , -maxY/10);
+      fillText(i.toFixed(2) + '', i, -maxY/8);
+    }
+    for (let i=-2*maxY; i <  maxY; i = i + 0.5) {
+      moveTo(maxX/10, i);
+      lineTo(-maxX/10, i);
+      fillText(i+'', maxX/8, i);
+    }
     ctx.stroke();
-
-
-
-    //ctx.beginPath(); //нули функции и вершины функции
-    //ctx.fillStyle = 'orange';
-    //ctx.arc (355.5, 179.5, 2, 0, Math.PI * 2, false); // pi
-    //ctx.arc (318.25, 179.5, 2, 0, Math.PI * 2, false); // pi/2
-    //ctx.fill();
-
-
   }
-
 }
-
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.mainControls {
+  width: 300px;
+  height: 600px;
+  background-color: #89c7d2;
+  border-radius: 80px;
+  float: left;
+  display: inline;
+}
 
+.mainCanvas {
+  width: 600px;
+  height: 600px;
+  float: left;
+  display: inline;
+}
 </style>
+
+
