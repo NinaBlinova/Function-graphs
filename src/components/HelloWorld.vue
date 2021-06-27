013b664be8101a0a35952559d83c2a96f439e3a0
Pavel Blinov <blinov.pavel@gmail.com>
2021-06-27 16:29:48 +0300

    Function definition

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,10 +3,18 @@
     <h1>{{ msg }}</h1>
 
     <div id="rectangle" class="mainControls">
-      <button v-on:click="drawRect">Function</button>
-      <label for="maxX">Max X (* pi)</label><input v-model="maxX" id="maxX" type="number">
-      <label for="maxY">Max Y</label><input v-model="maxY" id="maxY" type="number">
-      <label for="fn">Max Y</label><input v-model="fn" id="fn">
+
+      <label for="fn" class="CharacteristicsOfTheText1">function</label>
+      <input v-model="fn" id="fn" class="EnteringValues1">
+
+      <label for="maxX" class="CharacteristicsOfTheText2">Max X (* pi)</label>
+      <input v-model="maxX" id="maxX" type="number" class="EnteringValues2">
+
+      <label for="maxY" class="CharacteristicsOfTheText3">Max Y</label>
+      <input v-model="maxY" id="maxY" type="number" class="EnteringValues3">
+
+      <button v-on:click="drawRect" class="Butt">Function</button>
+
     </div>
     <canvas id="canvas" width="600" height="600" class="mainCanvas"></canvas>
   </div>
@@ -63,6 +71,10 @@ export default class HelloWorld extends Vue {
       const bY = canvasHeight / 2;
       return -convertHeight(y) + bY
     }
+    const crossing = function (x1: number, y1: number, x2: number, y2: number): number{
+      return x2 - marginX*(x2 - x1)/(y2 - y1);
+    }
+
 
 
     const rect = function (x: number, y: number, width: number, height: number): void {
@@ -77,6 +89,32 @@ export default class HelloWorld extends Vue {
     const fillText = function (str: string, x: number, y: number): void {
       ctx.fillText(str, convertX(x), convertY(y));
     }
+
+    const IntersectionOfLines = function (x1: number, y1: number, x2: number, y2: number): void{
+      if (y2 < maxY && y2 > -maxY && y1 < maxY && y1 > -maxY)
+      {
+        moveTo(x1,y1);
+        lineTo(x2,y2);
+      }
+      else if (y2 > maxY && y1 < maxY && y1 > -maxY) // 1 and 2 fourth
+      {
+        moveTo(x1,y1);
+        lineTo((maxY - y1)*(x1 - x2) / (y1 - y2) + x1, maxY)
+      }
+      else if (y2 < maxY && y1 > maxY && y2 > -maxY) {
+        moveTo(x2,y2);
+        lineTo((maxY - y2)*(x2 - x1) / (y2 - y1) + x2, maxY)
+      }
+      else if (y2 < -maxY && y1 > -maxY && y1 < maxY) { // 3 and 3 fourth
+        moveTo(x1,y1);
+        lineTo((-maxY - y1)*(x1 - x2) / (y1 - y2) + x1, -maxY)
+      }
+      else if ( y1 < -maxY && y2 > -maxY && y2 < maxY) { // 3 and 3 fourth
+        moveTo(x2,y2);
+        lineTo((-maxY - y2)*(x2 - x1) / (y2 - y1) + x2, -maxY)
+      }
+    }
+
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
     ctx.beginPath();
@@ -108,36 +146,38 @@ export default class HelloWorld extends Vue {
       moveTo(-maxX, y1);
       lineTo(maxX, y1);
     }
-
     ctx.stroke();
 
     //график
     ctx.beginPath();
     ctx.lineWidth = 3;
     ctx.strokeStyle = '#29a0a4';
-    moveTo(-maxX,0);
     let x = 0;
     let y = 0;
-    for (x = -maxX; x <= maxX; x = x+1/maxX)
+    moveTo(-maxX,fn(-maxX));
+    for (x = -maxX; x < maxX; x = x+1/maxX)
     {
       y = fn(x);
-      lineTo(x,y);
-    }
+      console.info('y = ');
+      IntersectionOfLines(x, y, x+1/maxX, fn(x+1/maxX))
+
+      }
     ctx.stroke();
 
     //название осей
+
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#08191c";
     ctx.font = '10px cursive';
-    for (let i = -2*maxX; i < maxX; i = i + Math.PI/2) {
-      moveTo(i, maxY/10);
-      lineTo(i , -maxY/10);
-      fillText(i.toFixed(2) + '', i, -maxY/8);
+    for (let i = -2*maxX; i < maxX; i = i + Math.PI) {
+      moveTo(i, maxY/20);
+      lineTo(i , -maxY/20);
+      fillText(i.toFixed(2), i, -maxY/8);
     }
-    for (let i=-2*maxY; i <  maxY; i = i + 0.5) {
-      moveTo(maxX/10, i);
-      lineTo(-maxX/10, i);
+    for (let i=-2*maxY; i <  maxY; i = i + 1) {
+      moveTo(maxX/20, i);
+      lineTo(-maxX/20, i);
       fillText(i+'', maxX/8, i);
     }
     ctx.stroke();
@@ -150,9 +190,49 @@ export default class HelloWorld extends Vue {
   width: 300px;
   height: 600px;
   background-color: #89c7d2;
-  border-radius: 80px;
+  border-radius: 70px;
   float: left;
   display: inline;
+  position: relative;
+  text-align: left;
+  font-family: cursive;
+}
+
+.CharacteristicsOfTheText1{
+  left: 5px;
+  position: absolute;
+  top: 48px;
+  display: table-caption;
+}
+.CharacteristicsOfTheText2{
+  left: 5px;
+  position: absolute;
+  top: 96px;
+  display: table-caption;
+}
+.CharacteristicsOfTheText3{
+  left: 5px;
+  position: absolute;
+  top: 144px;
+  display: table-caption;
+}
+
+.EnteringValues1 {
+  margin-top: 18%;
+  float: right;
+  border-radius: 40px;
+}
+
+.EnteringValues2 {
+  margin-top: 8%;
+  float: right;
+  border-radius: 40px;
+}
+
+.EnteringValues3 {
+  margin-top: 9%;
+  float: right;
+  border-radius: 40px;
 }
 
 .mainCanvas {
@@ -161,6 +241,24 @@ export default class HelloWorld extends Vue {
   float: left;
   display: inline;
 }
+
+
+.Butt {
+  font-family: cursive;
+  float: right;
+  position: absolute;
+  bottom: 0px;
+  left: 38%;
+  display:block;
+  color: #244950;
+  text-decoration: none;
+}
+.Butt:visited {
+  color: #68b1bf;
+}
+.Butt:hover {
+  color: #2b3c62;
+  box-shadow: inset 0 0 0 23px #5e9fb4;
+}
+
 </style>
-
-
